251f7b70421f12854dd113c3b2b682b9f294b113
Fede Estrubia <estrubia455@gmail.com>
2023-01-13 15:42:22 -0300

    JS-IV 2/17completed 13/1/2023

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,14 @@ function crearGato (nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+
+  var gato = {
+    nombre: nombre,
+    edad: edad,
+  }
+
+  function meow(){return ('Meow!')}
+  return (gato)
 }
 
 function agregarPropiedad (objeto, property) {
@@ -13,20 +21,26 @@ function agregarPropiedad (objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+var objeto = {}
+  objeto.property = 'null'
+  return (objeto)
 }
 
 function invocarMetodo (objeto, metodo) {
-  // "metodo" es una cadena que contiene el nombre de un método (funcion) en el objeto
+  // "metodo" es una string que contiene el nombre de un método (funcion) en el objeto
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
-}
+  }
 
 function multiplicarNumeroDesconocidoPorCinco (objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
-
+  var n = objetoMisterioso;
+  objetoMisterioso.numeroMisterioso = n;
+  var r = n * 5;
+ console.log(r)
 }
 
 function eliminarPropiedad (objeto, unaPropiedad) {
@@ -40,6 +54,13 @@ function nuevoUsuario (nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
+  var objeto ={
+    nombre: nombre,
+    email: email,
+    password: password,
+  }
+  return (objeto);
+
 
 }
 
@@ -47,6 +68,9 @@ function tieneEmail (usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
+
+    return(usuario.hasOwnProperty(email[prop]))
+
 }
 
 function tienePropiedad (objeto, propiedad) {
@@ -54,6 +78,13 @@ function tienePropiedad (objeto, propiedad) {
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (objeto = {
+    propiedad: propiedad,
+}) {return (true)}
+else return(false)
+    //esta mal
+
+
 }
 
 function verificarPassword (usuario, password) {
@@ -61,6 +92,7 @@ function verificarPassword (usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // Tu código:
+
 }
 
 function actualizarPassword (usuario, nuevaPassword) {
